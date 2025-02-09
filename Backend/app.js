ac7c3fe93d34a0322b0a7c86a8547272f3b1694f
shubhamsingh <shubhamsingh2142@gmail.com>
2025-02-10 01:09:54 +0530

    UserModule Created

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,8 @@ const express=require("express")
 const cors=require('cors')
 const app=express()
 app.use(cors());
+const connectToDB = require('./DB/db.js')
+connectToDB()
 
 
 app.get('/', (req, res)=>{
